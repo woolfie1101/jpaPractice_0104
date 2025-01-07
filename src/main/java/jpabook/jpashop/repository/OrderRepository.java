dd83dcb027849b9c538d46d2f1ce27b8259e5f5c
kimjoohee <92443218+woolfie1101@users.noreply.github.com>
2025-01-07 17:00:00 +0900

    주문 레파지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    // public List<Order> findAll(OrderSearch orderSearch){}
+}
